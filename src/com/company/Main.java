c2070568a9120f2f3595c57dd95ac15af4dee214
Garrett McGuire <garrettcmcguire@gmail.com>
2021-08-14 09:48:34 -0400

    comments added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        //create Scanner and Variables
+
         Scanner Keyboard = new Scanner(System.in);
         double unitPrice;
         int quantity;
         boolean taxable;
 
+        //set Number Format to currency ($) American USD
+
         NumberFormat currency = NumberFormat.getCurrencyInstance();
+
+        //prompt user for unit price, quantity, and taxable. Collect answers
 
         System.out.println("Unit price: ");
         unitPrice = Keyboard.nextDouble();
@@ -19,16 +26,20 @@ public class Main {
         System.out.println("Taxable? True/False");
         taxable = Keyboard.nextBoolean();
 
+        //algorithm to solve for total
+
         double total = unitPrice * quantity;
         if (taxable) {
             total = total * 1.05;
         }
+
+        //output of the amount, now shows the $ in front of total
 
         String niceTotal = currency.format(total);
         System.out.println("Total: ");
         System.out.println(niceTotal);
 
         Keyboard.close();
-        
+
     }
 }
